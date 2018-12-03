c072808d9c88ac9038c8070a72bb49a33e34cc41
Darren Steadman <darren.steadman@irisys.co.uk>
2018-12-03 14:43:44 +0000

    first commit

---
+++ HTTPPushDemo/Controllers/IrisysController.cs
@@ -0,0 +1,55 @@
+﻿
+namespace HTTPPushDemo.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Newtonsoft.Json.Linq;
+    using System;
+
+    /// <summary>
+    /// Web API controller to handle HTTP post from devices
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IrisysController : ControllerBase
+    {
+        /// <summary>
+        /// Handles data being posted from the device
+        /// </summary>
+        /// <param name="value">JSON containing the data</param>
+        [HttpPost("count_data")]
+        public void CountData([FromBody] JToken value)
+        {
+            // Get the serial number of the device sending the data
+            var deviceId = value["FriendlyDeviceSerial"];
+
+            Console.WriteLine("Got data for device " + deviceId);
+
+            // Loop around each count log entry
+            foreach (var countData in value["CountLogs"])
+            {
+                // Extract the log entry ID and the timestamp for when the log entry occured (Note all timestamps are UTC)
+                var logEntryID = (long)countData["LogEntryId"];
+                var timestamp = (string)countData["Timestamp"];
+
+                Console.WriteLine("Log entry: " + logEntryID + " at time: " + timestamp);
+
+                // Loop around each count value in this entry
+                foreach (var count in countData["Counts"])
+                {
+                    // Name of the register the data is associated with
+                    var registerName = (string)count["Name"];
+
+                    // ID of the register
+                    var registerID = (long)count["RegisterId"];
+
+                    // Count value
+                    var countValue = (long)count["Value"];
+
+                    Console.WriteLine("Register: " + registerName + " ID: " + registerID + " value: " + countValue);
+                }
+            }
+
+            Console.WriteLine("Data complete");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+Vector HTTP POST example
+
+This application gives a simple example of how to parse the JSON data that is sent from a Vector device.
+
+Requires VS 2017 .NET 4.6.1 ASP.NET Core 2.1
+
+Please refer to demodata.json for the format of the data.
+
+To use this demo you will need a correctly configured Vector device or access to EM.
